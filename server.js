49b5bd36c4b543c8863f57386459330aa0b24654
alexrevell <revellalex@gmail.com>
2015-12-31 10:20:08 +1300

    add stream set list to server

--- server.js
+++ server.js
@@ -1,13 +1,28 @@
 var net = require('net')
+var streamSet = require('stream-set')
+
+var activeSockets = streamSet()
 
 var server = net.createServer(function(socket){
   console.log("client connected")
+
+  activeSockets.add(socket)
+  console.log("active sockets: ", activeSockets.size)
+
+  socket.on('close', function(){
+    console.log("active sockets: ", activeSockets.size)
+  })
+
   socket.on('data', function(data){
     socket.write(data)
   })
 })
 
 server.listen(8124, function(){
-  var address = server.address()
-  console.log("server bound, listening on ", address)
+  console.log("server bound, listening on ", server.address)
+  var socket = net.connect(8124)
+  socket.on('connect', function(){
+    console.log("socket connected")
+    socket.destroy()
+  })
 })
