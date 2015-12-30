32f1fb653c1f7ca893475722f388871492384b4a
alexrevell <revellalex@gmail.com>
2015-12-31 11:16:15 +1300

    sockets communicate to server, server talks to all socket connections

--- server.js
+++ server.js
@@ -4,8 +4,6 @@ var streamSet = require('stream-set')
 var activeSockets = streamSet()
 
 var server = net.createServer(function(socket){
-  console.log("client connected")
-
   activeSockets.add(socket)
   console.log("active sockets: ", activeSockets.size)
 
@@ -13,16 +11,14 @@ var server = net.createServer(function(socket){
     console.log("active sockets: ", activeSockets.size)
   })
 
-  socket.on('data', function(data){
+  process.stdin.on('data', function(data){
     socket.write(data)
+  })
+
+  socket.on('data', function(data){
+    process.stdout.write(data)
   })
 })
 
-server.listen(8124, function(){
-  console.log("server bound, listening on ", server.address)
-  var socket = net.connect(8124)
-  socket.on('connect', function(){
-    console.log("socket connected")
-    socket.destroy()
-  })
-})
+server.listen(8124)
+
