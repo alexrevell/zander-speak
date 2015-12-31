2c9ec16b622dbc887be983551fcdb7c7f4643020
alexrevell <revellalex@gmail.com>
2015-12-31 20:27:51 +1300

    peer messaging with username

--- client.js
+++ client.js
@@ -1,6 +1,5 @@
 var lookup = require('lookup-multicast-dns')
 // lookup('zander-speak.local')
-
 var net = require('net')
 var jsonStream = require('duplex-json-stream')
 var socket = jsonStream(net.connect(8124, 'zander-speak.local'))
--- peer.js
+++ peer.js
@@ -1,10 +1,51 @@
+var net = require('net')
+var jsonStream = require('duplex-json-stream')
 var topology = require('fully-connected-topology');
+var streamSet = require('stream-set')
 
-var me = process.argv[2]
-var peers = process.argv.slice(3)
+var username = process.argv[2]
+var me = process.argv[3]
+var peers = process.argv.slice(4)
 
-var mine = topology(me, peers)
+var activeSockets = streamSet()
+var swarm = topology(me, peers)
 
-mine.on('connection', function(connection, peer){
-  console.log("I am connected to...", peer)
+swarm.on('connection', function(socket, id){
+  socket = jsonStream(socket)
+  activeSockets.add(socket)
+  console.log("I am connected to...", id)
+  console.log("active sockets: ", activeSockets.size)
+
+  socket.on('close', function(){
+    console.log("active sockets: ", activeSockets.size)
+  })
+
+  socket.on('data', function(data){
+    console.log(data.username + "> " + data.message)
+
+    // var senderIndex = getIndex(socket)
+    // var recipients = activeSockets.streams.filter(function(stream){
+    //   return matchIndex(senderIndex, stream)
+    // })
+
+    // recipients.forEach(function(stream){
+    //   stream.write({ username: data.username, message: data.message })
+    // })
+  })
+
+  process.stdin.on('data', function(data){
+    socket.write({ username: username, message: data.toString().trim() })
+  })
+
+  // swarm.on('data', function(data){
+  //   console.log(data)
+  // })
 })
+
+var getIndex = function(stream){
+  return activeSockets.streams.indexOf(stream)
+}
+
+var matchIndex = function(index, stream){
+  return getIndex(stream) !== index
+}
